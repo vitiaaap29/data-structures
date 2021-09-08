7e23c573b6c3697df1bc5020feddf6534f67ad88
Viktor Punko <viktor.punko@effective-soft.com>
2021-09-08 22:14:16 +0300

    Fix queue grow structure t -> i1 -> i2 -> h

--- lists/linked-list-queue.js
+++ lists/linked-list-queue.js
@@ -3,27 +3,56 @@
  * FIFO list.
  */
 
-import { LinkedListItem } from '../lib/linked-list-item.js';
 import { LinkedListStack } from './linked-list-stack.js';
+import { LinkedListItem } from '../lib/linked-list-item.js';
 
 class LinkedListQueue {
   constructor() {
     this.head = null;
+    this.tail = null;
   }
 
   // O(1)
   insertToHead(value) {
-    return LinkedListStack.prototype.insertToHead.call(this, value);
+    const newItem = new LinkedListItem(value);
+    if (this.tail === null) {
+      this.tail = newItem;
+      this.head = this.tail;
+    } else {
+      const oldHead = this.head;
+      oldHead.next = newItem;
+      this.head = newItem;
+    }
   }
 
   // O(n)
   printAllItems() {
-    return LinkedListStack.prototype.printAllItems.call(this);
+    for (let index = 0, currentTail = this.tail; currentTail !== null; index++) {
+      console.log(index, currentTail.value);
+      currentTail = currentTail.next;
+    }
   }
 
   // O(1)
-  removeFromTail() {
+  removeAndGetFirstInItem() {
+    if (this.tail === null) {
+      return null;
+    }
+    const currentTail = this.tail;
+    this.tail = this.tail.next;
 
+    return currentTail.value;
+  }
+
+  get size() {
+    let currentTail = this.tail;
+    let length = 0;
+    while (currentTail !== null) {
+      length += 1;
+      currentTail = currentTail.next;
+    }
+
+    return length;
   }
 }
 
@@ -31,12 +60,13 @@ const linkedList = new LinkedListQueue();
 linkedList.insertToHead('FirstInItem');
 linkedList.insertToHead('TheSecondInItem');
 linkedList.insertToHead('3rdInItem');
-linkedList.printAllItems();
+console.log('Size of queue', linkedList.size);
+// linkedList.printAllItems();
 
 // const indexSecondItem = linkedList.search('FirstInItem');
 // console.log('FirstInItem index is ', indexSecondItem);
 
-const lastInItem = linkedList.removeFromTail();
-console.log('Last In Stack item', lastInItem, 'current stack size is ', linkedList.size);
+const firstInItem = linkedList.removeAndGetFirstInItem();
+console.log('First In Queue item', firstInItem, ', current queue size is ', linkedList.size);
 
 linkedList.printAllItems();
