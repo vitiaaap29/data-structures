2a0273e745a5b49aa7a63e22d5301cac2b17a769
Viktor Punko <viktor.punko@effective-soft.com>
2021-09-08 23:47:50 +0300

    Queue remove and get first item test

--- tests/lists/linked-list-queue.test.js
+++ tests/lists/linked-list-queue.test.js
@@ -3,9 +3,16 @@ import { LinkedListQueue } from '../../lists/linked-list-queue.js';
 describe('Linked list Queue', () => {
 
   const queue = new LinkedListQueue();
+  const FIRST_IN_ITEM = 301;
 
   test('should queue has size after insert an item', () => {
-    queue.insertToHead(34);
+    queue.insertToHead(FIRST_IN_ITEM);
     expect(queue.size).toBe(1);
+  });
+
+  test('should queue be empty after remove all items', () => {
+    const firstInItem = queue.removeAndGetFirstInItem();
+    expect(firstInItem).toBe(FIRST_IN_ITEM);
+    expect(queue.size).toBe(0);
   });
 });
